6b0ebde104647db5fd68875ee811349888033878
diegonhalle <impellizzeri.diego@st.istitutomaserati.edu.it>
2025-01-30 18:12:45 +0100

    Add files via upload

---
+++ Putinator/json/questions.js
@@ -0,0 +1,11 @@
+const questions = [
+    { id: "1", question: "È vissuto prima del XIX secolo?", type: "era", value: "pre-19" },
+    { id: "2", question: "È vissuto nel XIX secolo?", type: "era", value: "19th" },
+    { id: "3", question: "È vissuto nel XX secolo?", type: "era", value: "20th" },
+    { id: "4", question: "È ancora vivo?", type: "alive", value: false },
+    { id: "5", question: "Fa parte delle STEM o dell'arte?", type: "field", value: "STEM" },
+    { id: "6", question: "È una donna?", type: "gender", value: "female" },
+    { id: "7", question: "È nato in Europa?", type: "region", value: "Europe" },
+    { id: "8", question: "È nato negli USA?", type: "region", value: "North America" }
+  ];
+  
